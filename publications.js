92a51c42169959c2b45de8253333d24331065b58
samuelscratch <kevinsamalex@gmail.com>
2022-07-19 19:06:22 +0200

    correction console log

--- publications.js
+++ publications.js
@@ -5,7 +5,6 @@ var app = angular.module('Arapp', []);
 app.controller('PublicationsController', function($scope, $http) {
   $http.get("./publications.php?cmd=getpublications")
   .then(function(response) {
-    console.log(response.data)
     $scope.years = response.data.years;
     $scope.publications = response.data.publications;
   });
